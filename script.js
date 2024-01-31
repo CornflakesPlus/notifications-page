2df2a32b05ed977c93dd3335c2f340da1839833b
kamran <kamranshahbad@gmail.com>
2024-01-31 14:01:31 +0400

    small changes with image

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ const messageList = [
 ];
 
 const imageArray = [
-  '../img1.png',
-  '../img2.png',
-  '../img3.png',
-  '../img4.png',
-  '../img5.png',
-  '../img6.png',
-  '../img7.png',
+  'images/img1.png',
+  'images/img2.png',
+  'images/img3.png',
+  'images/img4.png',
+  'images/img5.png',
+  'images/img6.png',
+  'images/img7.png',
 ];
 
 function formEvents(childNode, formInputNumber, message, image) {
